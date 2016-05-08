e52fa24b678028e2c3330db5dcc055e8263729f4
Thibault Friedrich <tf@elqui.fr>
2016-05-08 19:11:06 +0200

    add opening detector project

---
+++ opening_detector/README.md
@@ -0,0 +1,22 @@
+# Opening detector
+
+This system detects a box opening and emit an alarm when the box is opened and stop the alarm only when the box is closed.
+
+This project is using plateformio (see more [here](http://plateformio.org))
+
+## Hardware
+
+* arduino Uno
+* active buzzer: HCM1212X-1 Electro-Magnetic Sound Generator (see more [here](http://www.microbuzzer.com/buzzer-magnetic-12vdc-12mm) )
+* light sensor: TSL2561 (see more [here](https://www.adafruit.com/products/439) )
+
+## Software
+
+The project need the TSL2561 library available here: https://github.com/adafruit/TSL2561-Arduino-Library
+Copy it in the folder lib.
+
+## Help
+
+* https://learn.adafruit.com/tsl2561/wiring
+* https://github.com/adafruit/TSL2561-Arduino-Library
+* http://www.microbuzzer.com/buzzer-magnetic-12vdc-12mm
---
+++ opening_detector/src/opening_detector.ino
@@ -0,0 +1,78 @@
+#include <Wire.h>
+#include "TSL2561.h"
+
+// Example for demonstrating the TSL2561 library - public domain!
+
+// connect SCL to analog 5
+// connect SDA to analog 4
+// connect VDD to 3.3V DC
+// connect GROUND to common ground
+// ADDR can be connected to ground, or vdd or left floating to change the i2c address
+
+// The address will be different depending on whether you let
+// the ADDR pin float (addr 0x39), or tie it to ground or vcc. In those cases
+// use TSL2561_ADDR_LOW (0x29) or TSL2561_ADDR_HIGH (0x49) respectively
+
+TSL2561 tsl(TSL2561_ADDR_FLOAT);
+
+// connect buzzer to output 7
+int speakerPin = 7;
+
+void setup(void) {
+
+  // Open serial port to get logs
+  Serial.begin(9600);
+
+  // Init lu
+  if (tsl.begin()) {
+    Serial.println("Found sensor");
+  } else {
+    Serial.println("No sensor?");
+    while (1);
+  }
+
+  // You can change the gain on the fly, to adapt to brighter/dimmer light situations
+  //tsl.setGain(TSL2561_GAIN_0X);         // set no gain (for bright situtations)
+  tsl.setGain(TSL2561_GAIN_16X);      // set 16x gain (for dim situations)
+
+  // Changing the integration time gives you a longer time over which to sense light
+  // longer timelines are slower, but are good in very low light situtations!
+  tsl.setTiming(TSL2561_INTEGRATIONTIME_13MS);  // shortest integration time (bright light)
+  //tsl.setTiming(TSL2561_INTEGRATIONTIME_101MS);  // medium integration time (medium light)
+  //tsl.setTiming(TSL2561_INTEGRATIONTIME_402MS);  // longest integration time (dim light)
+
+  pinMode (speakerPin, OUTPUT);//buzzer
+}
+
+void loop(void) {
+  // Simple data read example. Just read the infrared, fullspecrtrum diode
+  // or 'visible' (difference between the two) channels.
+  // This can take 13-402 milliseconds! Uncomment whichever of the following you want to read
+  uint16_t x = tsl.getLuminosity(TSL2561_VISIBLE);
+  //uint16_t x = tsl.getLuminosity(TSL2561_FULLSPECTRUM);
+  //uint16_t x = tsl.getLuminosity(TSL2561_INFRARED);
+
+  Serial.println(x, DEC);
+
+  // More advanced data read example. Read 32 bits with top 16 bits IR, bottom 16 bits full spectrum
+  // That way you can do whatever math and comparisons you want!
+  uint32_t lum = tsl.getFullLuminosity();
+  uint16_t ir, full;
+  ir = lum >> 16;
+  full = lum & 0xFFFF;
+  Serial.print("IR: "); Serial.print(ir);   Serial.print("\t\t");
+  Serial.print("Full: "); Serial.print(full);   Serial.print("\t");
+  Serial.print("Visible: "); Serial.print(full - ir);   Serial.print("\t");
+
+  int luminosity = tsl.calculateLux(full, ir);
+
+  Serial.print("Lux: "); Serial.println(luminosity);
+
+  if (luminosity > 50) { // 50 lux is a fixed intensity which may not working in all environments
+    analogWrite (speakerPin, HIGH); //start alarm
+  } else {
+    analogWrite (speakerPin, LOW); //stop alarm
+  }
+
+  delay(100);
+}
